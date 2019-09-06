9e4a9e5cf8b334804501ac742163a67b7cfedfa2
Connor Holly <connorholly11@gmail.com>
2019-09-06 11:19:06 -0400

    everything showing up on screen including api call

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    console.log('constructor working')
+    super()
+    this.state = {
+      hello:{}
+    }
+
+    
+
+    }
+  componentDidMount(){
+    console.log('CDM WORKING')
+    axios
+      .get('http://localhost:5000/api/players')
+      .then((response) => console.log(response))
+      .then(response => this.setState({hello:response}))
+  }
+
+  render(){
+    console.log('render working')
+    return(
+      <div>
+        <h1>HELLO WORLD</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
